7ba184c659d37e3f3608816e2fefe849ca63879a
Esteban Gonzalez <esteban@estebandev.xyz>
2025-11-18 20:45:31 -0500

    init project

---
+++ src/test/java/com/poligran/tsp/sistema_encuestas/SistemaEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poligran.tsp.sistema_encuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
