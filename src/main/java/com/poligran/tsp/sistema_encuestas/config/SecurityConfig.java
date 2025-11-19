336109f9fb91b639d0d448425fdd3ae1fad712da
Esteban Gonzalez <esteban@estebandev.xyz>
2025-11-19 16:12:47 -0500

    feat: add initial security

---
+++ src/main/java/com/poligran/tsp/sistema_encuestas/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.poligran.tsp.sistema_encuestas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(Customizer.withDefaults())
+                .httpBasic(h -> h.disable())
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/admin"))
+                .authorizeHttpRequests(httz -> httz
+                        .requestMatchers("/").permitAll()
+                )
+                .build();
+
+    }
+}
